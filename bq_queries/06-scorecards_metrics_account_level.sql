d5d13ec8dc46b2405782acb16a49a3fa6f358a09
Gregory Fryns <gfryns@google.com>
2025-10-01 16:52:53 +0100

    bugfix: reorder BQ queries

--- bq_queries/06-scorecards_metrics_account_level.sql
+++ bq_queries/06-scorecards_metrics_account_level.sql
@@ -6,7 +6,7 @@ OR REPLACE TABLE `{bq_dataset}_bq.scorecards_metrics_account_level` AS (
       account_name,
       date,
       SUM(cost) AS cost
-    FROM `gfryns-dgpulse-demo.dgpulse_ads_bq.campaign_data`
+    FROM `{bq_dataset}_bq.campaign_data`
     GROUP BY account_id, account_name, date
   )
 
@@ -20,5 +20,5 @@ OR REPLACE TABLE `{bq_dataset}_bq.scorecards_metrics_account_level` AS (
     aca.ecl_status,
     if(aca.ecl_status='ENABLED', a.cost, null) AS cost_accounts_with_ecl
     FROM AccountsSpend AS a
-    LEFT JOIN `gfryns-dgpulse-demo.dgpulse_ads_bq.account_conversion_action` AS aca USING (account_id)
+    LEFT JOIN `{bq_dataset}_bq.account_conversion_action` AS aca USING (account_id)
 );
